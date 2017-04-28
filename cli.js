d4ba97a0a286c33c003b16cd50508682e35be542
Joshua Howarth <joshuahowarth@Joshuas-MacBook-Pro.local>
2017-04-28 16:23:43 +0100

    Update event emitter

--- cli.js
+++ cli.js
@@ -9,7 +9,7 @@ var CLI, EventEmitter, GITCLI, GitServer, Table, async, commander, certs, fs, ge
   __hasProp = {}.hasOwnProperty,
   __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
-EventEmitter = require('events').EventEmitter;
+EventEmitter = require('events');
 
 CLI = require('cli-listener');
 
